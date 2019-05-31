37bcd2f755449dc780c539c17fd9324d8d4717ad
Ignacio Paradisi <ignacioparadisi@gmail.com>
2019-05-31 16:08:29 -0400

    Feat: Add UIViewController extension

---
+++ Grades/Utils/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIView+Extension.swift
+//  Grades
+//
+//  Created by Ignacio Paradisi on 5/31/19.
+//  Copyright © 2019 Ignacio Paradisi. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
